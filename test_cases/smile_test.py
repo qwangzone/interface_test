0310dc5e0101c8d6b40b59e9e0ce3a983c7fd95a
qwangzone <578380132@qq.com>
2017-07-12 11:27:45 +0800

    代码优化

--- test_cases/smile_test.py
+++ test_cases/smile_test.py
@@ -17,42 +17,59 @@ class SmileTaskTestCase(unittest.TestCase):
 
     def test_get_task(self):
         """获取单个任务的详情"""
-        url = self.ip + "/1"
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/" + str(create_task_id)
         response = requests.request("GET", url)
         res = response.json()
         print (res)
-        self.assertEqual(res["title"], "test1")
-        self.assertEqual(res["desc"], "test1")
+        self.assertEqual(res["title"], "test")
+        self.assertEqual(res["desc"], "desc")
+        requests.request("DELETE", url)
 
     def test_put_task(self):
         """"完成一个任务"""
-        url = self.ip + "/2"
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/" + str(create_task_id)
         response = requests.request("PUT", url)
         res = response.json()
         print (res)
+        self.assertEqual(res["id"], create_task_id)
         self.assertEqual(res["done"], True)
+        requests.request("DELETE", url)
 
     def test_post_task(self):
         """"创建一个任务"""
-        payload = {"title": "wq", "desc": "wq"}
-        response = requests.post(self.ip,data=payload)
-        res = response.json()
-        print (res)
-        self.assertEqual(res["title"], "wq")
-        self.assertEqual(res["desc"], "wq")
-
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/" + str(create_task_id)
+        print (create_task_res)
+        self.assertEqual(create_task_res["title"], "test")
+        self.assertEqual(create_task_res["desc"], "desc")
+        requests.request("DELETE", url)
     def test_delete_task(self):
         """"删除一个任务"""
-        url = self.ip + "/6"
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/" + str(create_task_id)
         response = requests.request("DELETE", url)
         res = response.json()
         print (res)
-        self.assertEqual(res["id"], '6')
+        self.assertEqual(res["id"], str(create_task_id))
+
+    def create_task(self, title, desc):
+        url = self.ip
+        payload = json.dumps({'title': title, 'desc': desc})
+       # payload1 = {'title': title, 'desc': desc}
+        headers = {'content-type': "application/json"}
+        response = requests.request("POST", url, data=payload, headers=headers)
+        return response.json()
 
 
 if __name__ == '__main__':
-    unittest.main()
-    #suit = unittest.TestSuite()
-    #suit.addTest(SmileTaskTestCase('test_delete_task'))
-    #runner = unittest.TextTestRunner()
-    #runner.run(suit)
+    #unittest.main()
+    suit = unittest.TestSuite()
+    suit.addTest(SmileTaskTestCase('test_delete_task'))
+    runner = unittest.TextTestRunner()
+    runner.run(suit)
