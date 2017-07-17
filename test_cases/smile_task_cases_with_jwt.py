41aae702f941515296f5adcb5d5e0a3c42d106a4
qwangzone <578380132@qq.com>
2017-07-17 09:57:13 +0800

    加入jwt鉴权

--- test_cases/smile_task_cases_with_jwt.py
+++ test_cases/smile_task_cases_with_jwt.py
@@ -5,8 +5,16 @@ class SmileTaskTestCase(unittest.TestCase):
     def setUp(self):
         self.ip = 'http://127.0.0.1:3000'
         self.token = self.login('admin', 'admin')
-
+    """
+   #注册用户
+    def register(self, username, password, password_confirmation):
+        url = self.ip + "/register"
+        response = requests.request("POST", url, data={"username": username, "password": password, "password_confirmation" : password_confirmation})
+        res = response.json()
+        print (res)
+    """
     def login(self, username, password):
+        #self.register("admin", "admin", "admin")
         url = self.ip + "/login"
         res = requests.request("POST", url, data={'username': username, 'password': password}).json()
         print(res)
@@ -14,17 +22,19 @@ class SmileTaskTestCase(unittest.TestCase):
 
     @property
     def headers_with_jwt(self):
-        return {'Authrization': 'Bearer' + self.token, 'content-type': 'application/json'}
+        return {'Authorization': 'Bearer ' + self.token, 'content-type': 'application/json'}
 
     def create_task(self, title, desc):
         url = self.ip + "/api/tasks"
         payload =json.dumps({'title': title, 'desc': desc})
-        response = requests.request("POST", url, headers=self.headers_with_jwt)
+        response = requests.request("POST", url, data=payload, headers=self.headers_with_jwt)
         return response.json()
 
     def test_create_task(self):
         res = self.create_task('wq', 'wangqi')
+        print (res)
         self.assertEqual(res['title'], 'wq')
+
 if __name__ == '__main__':
     suit = unittest.TestSuite()
     suit.addTest(SmileTaskTestCase('test_create_task'))
