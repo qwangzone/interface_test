479b1813f1515b877177d780eafb0e1de090a493
qwangzone <578380132@qq.com>
2017-07-17 11:44:21 +0800

    完善代码

--- test_cases/smile_task_cases_with_jwt.py
+++ test_cases/smile_task_cases_with_jwt.py
@@ -34,9 +34,42 @@ class SmileTaskTestCase(unittest.TestCase):
         res = self.create_task('wq', 'wangqi')
         print (res)
         self.assertEqual(res['title'], 'wq')
+        self.assertEqual(res['desc'], 'wangqi')
 
+    def test_get_all_task(self):
+        url = self.ip + "/api/tasks"
+        res = requests.request("GET", url, headers=self.headers_with_jwt).json()
+        print (res)
+        self.assertNotEqual(res, '[]')
+
+    def test_get_task(self):
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/api/tasks/" + str(create_task_id)
+        res = requests.request("GET", url, headers=self.headers_with_jwt).json()
+        print (res)
+        self.assertEqual(res['title'], 'test')
+        self.assertEqual(res['desc'], 'desc')
+        requests.request("DELETE", url, headers=self.headers_with_jwt)
+
+    def test_delete_task(self):
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/api/tasks/" + str(create_task_id)
+        res = requests.request("DELETE", url, headers=self.headers_with_jwt).json()
+        print (res)
+        self.assertEqual(res['id'], str(create_task_id))
+
+    def test_put_task(self):
+        create_task_res = self.create_task("test", "desc")
+        create_task_id = create_task_res["id"]
+        url = self.ip + "/api/tasks/" + str(create_task_id)
+        res = requests.request("PUT", url, headers=self.headers_with_jwt).json()
+        print (res)
+        self.assertEqual(res['id'], create_task_id)
+        self.assertEqual(res['done'], True)
 if __name__ == '__main__':
     suit = unittest.TestSuite()
-    suit.addTest(SmileTaskTestCase('test_create_task'))
+    suit.addTest(SmileTaskTestCase('test_put_task'))
     runner = unittest.TextTestRunner()
     runner.run(suit)
